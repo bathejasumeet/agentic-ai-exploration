c654763ade62bd411b8b0dd817c0bb4a3ee00c47
Sumeet Batheja <bathejasumeet@gmail.com>
2026-04-09 20:51:16 +0200

    refactor

--- services/parallel_prompting_service.py
+++ services/parallel_prompting_service.py
@@ -10,7 +10,6 @@ from core.llms.local_ollama_llm_wrapper import LocalOllamaLLMWrapper
 class State(TypedDict):
     prompts: list[str]
     results: Annotated[list[str], add]
-    len: int
 
 
 class ParallelPromptingService:
@@ -23,6 +22,7 @@ class ParallelPromptingService:
             return {
                 'results': [result]
             }
+
         return node
 
     def aggregator(self, state: State):
@@ -30,13 +30,21 @@ class ParallelPromptingService:
         final_result = self.llm.get_model().invoke(f"combine the information and summarize it : {info}")
         return final_result
 
-    def create_parallel_prompting_graph(self, prompts: list[str]):
+    def create_parallel_prompting_graph(self, prompts_len: int):
         parallel_prompting_graph = StateGraph(State)
         parallel_prompting_graph.add_edge('aggregator', self.aggregator)
-        for i,prompt in enumerate(prompts):
+        for i in range(0, prompts_len):
             node_name = f'parallel_node_{i}'
             parallel_prompting_graph.add_node(node_name, self.get_parallel_node(i))
             parallel_prompting_graph.add_edge(START, node_name)
             parallel_prompting_graph.add_edge(node_name, 'aggregator')
         parallel_prompting_graph.add_edge('aggregator', END)
+        self.agent = parallel_prompting_graph.compile()
 
+    def invoke(self, prompts: list[str]):
+        self.create_parallel_prompting_graph(len(prompts))
+        result = self.agent.invoke({
+            'prompts': prompts,
+            'results': []
+        })
+        return result
