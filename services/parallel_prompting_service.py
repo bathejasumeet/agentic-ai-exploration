0da8a33a82557b1374bdb810ff9ceaf8707d4c3c
Sumeet Batheja <bathejasumeet@gmail.com>
2026-04-09 21:00:51 +0200

    refactor and test

--- services/parallel_prompting_service.py
+++ services/parallel_prompting_service.py
@@ -10,6 +10,7 @@ from core.llms.local_ollama_llm_wrapper import LocalOllamaLLMWrapper
 class State(TypedDict):
     prompts: list[str]
     results: Annotated[list[str], add]
+    final_result: str
 
 
 class ParallelPromptingService:
@@ -20,19 +21,21 @@ class ParallelPromptingService:
         def node(state: State):
             result = self.llm.get_model().invoke(state['prompts'][index])
             return {
-                'results': [result]
+                'results': [result.content]
             }
 
         return node
 
     def aggregator(self, state: State):
         info = '\n'.join(f'info {i} : {result}' for i, result in enumerate(state['results']))
-        final_result = self.llm.get_model().invoke(f"combine the information and summarize it : {info}")
-        return final_result
+        final_result = self.llm.get_model().invoke(f"combine the information and summarize it in a funny way finding a link between them : {info}")
+        return {
+            'final_result': final_result
+        }
 
     def create_parallel_prompting_graph(self, prompts_len: int):
         parallel_prompting_graph = StateGraph(State)
-        parallel_prompting_graph.add_edge('aggregator', self.aggregator)
+        parallel_prompting_graph.add_node('aggregator', self.aggregator)
         for i in range(0, prompts_len):
             node_name = f'parallel_node_{i}'
             parallel_prompting_graph.add_node(node_name, self.get_parallel_node(i))
@@ -48,3 +51,13 @@ class ParallelPromptingService:
             'results': []
         })
         return result
+
+if __name__ == '__main__':
+    prompts = [
+        "Write a cat joke",
+        "Write a dad fact",
+        "Give me one healthy saying for mind"
+    ]
+
+    parallel_prompting_service = ParallelPromptingService()
+    print(parallel_prompting_service.invoke(prompts))
